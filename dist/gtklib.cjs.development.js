e64196f3f20c566331c88d8b7b7c79d8bab3e70f
Guilherme Tomazi Klein <guilhermet.klein@procempa.com.br>
2023-10-25 11:03:09 -0300

    fsdfsd

---
+++ dist/gtklib.cjs.development.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var React = require('react');
+
+// Delete me
+var Thing = function Thing() {
+  return React.createElement("div", null, "the snozzberries taste like snozzberries");
+};
+
+exports.Thing = Thing;
+//# sourceMappingURL=gtklib.cjs.development.js.map
---
+++ dist/gtklib.esm.js
@@ -0,0 +1,9 @@
+import { createElement } from 'react';
+
+// Delete me
+var Thing = function Thing() {
+  return createElement("div", null, "the snozzberries taste like snozzberries");
+};
+
+export { Thing };
+//# sourceMappingURL=gtklib.esm.js.map
